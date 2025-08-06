adc9cd36478c83626376c9e39d3d5b41e1f7d6fb
zonic-ultra <zonicultra@gmail.com>
2025-08-07 02:36:07 +0700

    adding security

--- ems-client/src/api/axios.jsx
+++ ems-client/src/api/axios.jsx
@@ -1,7 +1,9 @@
+// src/services/api.js
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.API_BASE_URL,
+  baseURL: process.env.REACT_APP_API_BASE_URL,
+  validateStatus: () => true, // ⬅️ You can include this globally
 });
 
 export default api;
