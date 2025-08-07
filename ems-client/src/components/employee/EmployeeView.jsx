d7d03e7ba330d00a26ac332c2456dcea51c87981
zonic-ultra <zonicultra@gmail.com>
2025-08-07 14:39:51 +0700

    adding env on vercel02

--- ems-client/src/components/employee/EmployeeView.jsx
+++ ems-client/src/components/employee/EmployeeView.jsx
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 import Search from "../common/Search";
 // import api from "../../api/axios";
 import axios from "axios";
-import api from "../../api/axios";
 
 const EmployeeView = () => {
   const [employees, setEmployees] = useState([]);
@@ -19,9 +18,12 @@ const EmployeeView = () => {
   }, []);
 
   const loadEmployees = async () => {
-    const result = await api.get("/api/employees/all", {
-      validateStatus: () => true,
-    });
+    const result = await axios.get(
+      "https://employee-management-system-version1.onrender.com/api/employees/all",
+      {
+        validateStatus: () => true,
+      }
+    );
     if (result.status === 302) {
       setEmployees(result.data);
     }
