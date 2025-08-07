47bc0f152715b373662a5cd786ecd5d228c00867
zonic-ultra <zonicultra@gmail.com>
2025-08-07 14:15:08 +0700

    adding env on vercel

--- ems-client/src/api/axios.jsx
+++ ems-client/src/api/axios.jsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.API_BASE_URL,
+  baseURL: process.env.REACT_APP_API_BASE_URL,
   // validateStatus: () => true, // ⬅️ You can include this globally
 });
 
